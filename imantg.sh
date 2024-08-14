2017d24b071aaa56058a687713c7f3ee935b09a5
jetstreamin <mmahon512@gmail.com>
2024-08-14 17:30:38 -0500

    init

---
+++ imantg.sh
@@ -0,0 +1,8 @@
+while true; do \
+  for i in {1..11}; do clear && echo -e "\033[31m$(figlet -w 48 'I''mma')\033[0m"; sleep 0.3; clear && echo -e "\033[32m$(figlet -w 48 'Never Trump')\033[0m"; sleep 0.3; clear && echo -e "\033[34m$(figlet -w 48 'Guy!')\033[0m"; sleep 0.3; done; \
+  for i in {1..5}; do clear && echo -e "\033[34m$(figlet -w 48 'Guy!')\033[0m"; sleep 0.3; clear && echo -e "\033[32m$(figlet -w 48 'Never Trump')\033[0m"; sleep 0.3; clear && echo -e "\033[31m$(figlet -w 48 'I''mma')\033[0m"; sleep 0.3; done; \
+  for i in {1..3}; do clear && echo -e "\033[31m$(figlet -w 48 'I''mma')\033[0m"; sleep 0.3; clear && echo -e "\033[32m$(figlet -w 48 'Never Trump')\033[0m"; sleep 0.3; clear && echo -e "\033[34m$(figlet -w 48 'Guy!')\033[0m"; sleep 0.3; done; \
+  for i in {1..13}; do clear && echo -e "\033[33m$(figlet -w 48 'Harris / Walz')\033[0m"; sleep 0.3; done; \
+  for i in {1..3}; do clear && echo -e "\033[31m$(figlet -w 48 'USA')\033[0m"; sleep 0.3; clear && echo -e "\033[37m$(figlet -w 48 'USA')\033[0m"; sleep 0.3; clear && echo -e "\033[34m$(figlet -w 48 'USA')\033[0m"; sleep 0.3; done; \
+  for i in {1..7}; do clear && echo -e "\033[32m    ||||     \033[0m" && echo -e "\033[32m  /||||\\    \033[0m" && echo -e "\033[32m   ||||     \033[0m" && echo -e "\033[32m   ||||     \033[0m" && echo -e "\033[32m   ||||     \033[0m" && echo -e "\033[32m   \\\\//     \033[0m" && echo -e "\033[33m   ||||     \033[0m"; sleep 0.3; done; \
+done
